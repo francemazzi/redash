f5d70d25f3e645f94360745c4d7caa6803b8faf6
FraSma <francemazzi@gmail.com>
2023-01-03 11:05:59 +0100

    feat: template blues

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#2D9CDB",
-        secondary: "#27AE60",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
